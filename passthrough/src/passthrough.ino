502c617b0aca0790a8ca9de0cc19e6f1a5a966a0
Alessandro Basile <alessandro.basile.sr@gmail.com>
2026-01-21 12:55:26 +0100

    updated passthrough sketch for u-center testing

--- passthrough/src/passthrough.ino
+++ passthrough/src/passthrough.ino
@@ -1,34 +1,11 @@
-// Library for AXP20x Power Management
-#include <axp20x.h>
-
-#define GPS_RX 34 // RX on GPS module
-#define GPS_TX 12 // TX on GPS module
-
-AXP20X_Class axp;
-
-HardwareSerial GPSSerial1(1);
+#include "BoardSetup.h"
 
 void setup()
 {
   Serial.begin(115200);
 
-  Wire.begin(21, 22);
+  setupBoards();
 
-  if (!axp.begin(Wire, AXP192_SLAVE_ADDRESS))
-  {
-    Serial.println("[I] AXP192 Begin PASS");
-  }
-  else
-  {
-    Serial.println("[I] AXP192 Begin FAIL");
-  }
-  axp.setDCDC1Voltage(3300);                   // ESP32 3v3
-  axp.setLDO3Voltage(3300);                    // GPS   3v3
-  axp.setPowerOutPut(AXP192_DCDC1, AXP202_ON); // ESP32 ON
-  axp.setPowerOutPut(AXP192_LDO3, AXP202_ON);  // GPS   ON
-  axp.setPowerOutPut(AXP192_LDO2, AXP202_OFF); // LORA
-  axp.setPowerOutPut(AXP192_EXTEN, AXP202_OFF);
-  GPSSerial1.begin(9600, SERIAL_8N1, GPS_RX, GPS_TX);
 }
 
 void loop()
@@ -36,11 +13,11 @@ void loop()
 
   if (Serial.available())
   {
-    GPSSerial1.write(Serial.read()); // read it and send it out Serial1 (pins 0 & 1)
+    SerialGPS.write(Serial.read()); // read it and send it out Serial1 (pins 0 & 1)
   }
 
-  if (GPSSerial1.available())
+  if (SerialGPS.available())
   {
-    Serial.write(GPSSerial1.read()); // read it and send it out Serial (USB)
+    Serial.write(SerialGPS.read()); // read it and send it out Serial (USB)
   }
 }
